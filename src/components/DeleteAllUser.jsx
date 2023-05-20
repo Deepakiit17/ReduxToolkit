9eb4121a123d1aa2fcdcbc725f683fcab69d6af9
Deepak Meena <deepak.meena@ilearnplace.com>
2023-05-20 22:11:25 +0530

    complete code

--- src/components/DeleteAllUser.jsx
+++ src/components/DeleteAllUser.jsx
@@ -1,5 +1,26 @@
 import React from "react";
+import styled from "styled-components";
+import { clearAllUsers } from "../store/actions"
+import { useDispatch } from "react-redux";
 
-export const DeleteAllUser = () => {
-  return <div>DeleteAllUser</div>;
+const DeleteAllUser = () => {
+  const dispatch = useDispatch();
+
+  const deleteUsers = () => {
+    dispatch(clearAllUsers())
+  }
+
+  return <Wrapper>
+    <button className="btn clear-btn" onClick={deleteUsers}> Clear Users 
+    </button>
+  </Wrapper>;
 };
+
+const Wrapper = styled.section`
+.clear-btn{
+  text-transform: capitalize;
+  background-color: #db338a;
+}
+`
+
+export default DeleteAllUser;
